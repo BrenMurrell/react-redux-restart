c242ba3e350d0682073c8c38c27f80b1eab08bdc
BrenMurrell <bren.murrell@nvinteractive.com>
2018-04-09 11:49:35 +1200

    section 5 - add course form to manage page

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,26 +2,41 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
         super (props, context);
+
+        this.state = {
+            course: Object.assign({}, this.props.course),
+            errors: {}
+        };
     }
 
     render() {
         return (
-            <h1>Manage Course</h1>
+            <div>
+                <h1>Manage Course</h1>
+                <CourseForm 
+                    allAuthors={[]} //empty arry to start - get from API later
+                    course={this.state.course} 
+                    errors={this.state.errors}
+                />
+            </div>
         );
     }
 }
 
-ManageCoursePage.PropTypes = {
+ManageCoursePage.propTypes = {
     //myProp: PropTypes.string.isRequired
+    course: PropTypes.obj.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
+    let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: '' };
     return {
-        state: state
+        course: course
     };
 }
 
