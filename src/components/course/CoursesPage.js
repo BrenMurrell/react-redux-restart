9290d81784c613a95d4cd835464798e79067d6bf
BrenMurrell <bren.murrell@nvinteractive.com>
2018-04-09 10:02:32 +1200

    section 12 - get list into view

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
@@ -13,13 +14,15 @@ class CoursesPage extends React.Component {
         return <div key={index}>{course.title}</div>;
     }
     
-    
+
 
     render() {
+        const { courses } = this.props;
+
         return (
             <div>
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
+                <CourseList courses={courses} />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import configureStore from './store/configureStore';
 import { Provider } from 'react-redux'; //higher order component to attachs our store to our react container components. 
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import { loadCourses} from './actions/courseActions';
 import './styles/styles.css'; //webpack can import these too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore(); //can put initialState in as a function param if you're passing state from server / localStorage etc.
+
+store.dispatch(loadCourses());
 
 render (
     <Provider store={store}>
