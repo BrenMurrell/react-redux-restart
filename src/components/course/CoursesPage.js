20d3350d17179f34725f97dddb34195ec20d252f
BrenMurrell <bren.murrell@nvinteractive.com>
2018-04-09 09:26:48 +1200

    section 7 - add think middleware to store

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -7,44 +7,19 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
-        this.state = {
-            course: { title: "" }
-        };
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);    
+        
     }
     courseRow(course, index) {
         return <div key={index}>{course.title}</div>;
     }
+    
+    
 
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({ course: course });
-    }
-    onClickSave() {
-        //alert(`Saving ${this.state.course.title}`);
-        //this.props.dispatch(courseActions.createCourse(this.state.course)); <-- without mapDispatchToProps
-        //this.props.createCourse(this.state.course); //with it
-        this.props.actions.createCourse(this.state.course); //after adding bindActionCreators
-        
-    }
     render() {
         return (
             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}
-                <h2>Add course</h2>
-                <input 
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title}
-                />
-                <input 
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave}
-                />
             </div>
         );
     }
