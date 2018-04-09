0248445047d351883f95ffa0991e4f79559aa49f
BrenMurrell <bren.murrell@nvinteractive.com>
2018-04-09 16:06:04 +1200

    12.4 - create ajax status reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
+    courses, // <-- state.course in components. same as courses.courses. Aliasing courseReducer to make it easier to use in components
     authors,
-    courses // <-- state.course in components. same as courses.courses. Aliasing courseReducer to make it easier to use in components
+    ajaxCallsInProgress
 });
 
 export default rootReducer;
